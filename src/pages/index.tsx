e39d1e58da2e2892942985bade01e8a04cf9eda7
Yrslv Rzhkv <666anastyalena@mail.ru>
2024-08-19 17:32:44 +0300

    feat: add ssr props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import { Fragment, ReactElement } from 'react';
 import { Heading } from '@chakra-ui/react';
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 
 import { DefaultPageLayout } from '@/components/Layouts';
 
 import { NextPageWithLayout } from './_app';
 
-const Home: NextPageWithLayout = () => {
+interface HomePageProps {
+  heading: string;
+  testQuery?: string | string[];
+}
+
+const Home: NextPageWithLayout<HomePageProps> = ({ heading, testQuery }) => {
   return (
     <Fragment>
       <Heading>Home page test app</Heading>
+
+      <Heading>Srr data: {heading}</Heading>
+
+      {testQuery && <Heading>Query?: {testQuery}</Heading>}
     </Fragment>
   );
 };
@@ -18,3 +28,18 @@ Home.getLayout = function getLayout(page: ReactElement) {
 };
 
 export default Home;
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<HomePageProps>> {
+  const { test } = context.query;
+
+  const populatedWithPositionsProps: { props: HomePageProps } = {
+    props: {
+      heading: 'data from server',
+      testQuery: test
+    }
+  };
+
+  return { ...populatedWithPositionsProps };
+}
